1f99a0fc59f201349923b75b54f963e5f5f741ec
Bwaban <rayaalfareza7@gmail.com>
2026-02-26 23:44:18 +0700

    merubah warna background tambah

--- tambah.php
+++ tambah.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <style>
         body {
             font-family: Arial, sans-serif;
-            background: #f4f6f8;
+            background: #e6ecfe;
         }
 
         .container {
